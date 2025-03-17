791198e31b4026b7198f92f29acff711da0c68f5
Tumen-Amgalan <tumee2045@gmail.com>
2025-03-17 09:12:00 +0800

    Lab7
    B232270063

--- Java/src/main/java/frogger/Road.java
+++ Java/src/main/java/frogger/Road.java
@@ -9,10 +9,18 @@ public class Road {
     private final boolean[] occupied;
 
     public Road(boolean[] occupied) {
-        this.occupied = occupied;
+        this.occupied = occupied.clone();
     }
 
-    public boolean[] getOccupied() {
-        return this.occupied;
+    public boolean isValid(int position) {
+        return position >= 0 && position < occupied.length;
+    }
+
+    public boolean isOccupied(int position) {
+        return isValid(position) && occupied[position];
+    }
+
+    public int getLength() {
+        return occupied.length;
     }
 }
